0edd0fcc263664a1b20a1ae542cd88cdfbaf5d2d
erabbit <rabbitom@gmail.com>
2021-07-14 11:36:23 +0800

    feat: 获取传感器特征

--- src/main/java/net/erabbit/ble/BLESensor.java
+++ src/main/java/net/erabbit/ble/BLESensor.java
@@ -3,6 +3,10 @@ package net.erabbit.ble;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 
+import net.erabbit.ble.utils.LogUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
@@ -23,5 +27,22 @@ public class BLESensor extends BleDevice {
     public BLESensor(Context context, BluetoothDevice device, JSONObject jsonObject) {
         super(context, device, jsonObject);
         features = new ArrayList<>();
+        try {
+            JSONArray servicesArray = jsonObject.getJSONArray("services");
+            for (int s = 0; s < servicesArray.length(); s++) {
+                JSONObject serviceObject = servicesArray.getJSONObject(s);
+                JSONArray characteristicsArray = serviceObject.getJSONArray("characteristics");
+                for (int c=0; c<characteristicsArray.length(); c++) {
+                    JSONObject characteristicObject = characteristicsArray.getJSONObject(c);
+                    if(characteristicObject.getString("function").equals("feature")) {
+                        BLESensorFeature feature = new BLESensorFeature(characteristicObject);
+                        features.add(feature);
+                    }
+                }
+            }
+        }
+        catch(JSONException jsonException) {
+            LogUtil.e("sensor", jsonException.getMessage());
+        }
     }
 }
--- src/main/java/net/erabbit/ble/BLESensorFeature.java
+++ src/main/java/net/erabbit/ble/BLESensorFeature.java
@@ -1,6 +1,21 @@
 package net.erabbit.ble;
 
+import net.erabbit.ble.utils.LogUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class BLESensorFeature {
+
+    public BLESensorFeature(JSONObject jsonObject) {
+        try {
+            this.name = jsonObject.getString("name");
+            this.dimension = jsonObject.getInt("dimension");
+        }
+        catch(JSONException jsonException) {
+            LogUtil.e("feature", jsonException.getMessage());
+        }
+    }
 
     private boolean enabled = false;
 
