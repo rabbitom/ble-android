3bf662688f34f061536ec75a155261af7e9815ee
Tom <rabbitom@gmail.com>
2024-06-02 16:40:27 +0800

    feature: csl decode

--- src/androidTest/java/net/erabbit/csl/CSLTest.java
+++ src/androidTest/java/net/erabbit/csl/CSLTest.java
@@ -1,6 +1,5 @@
 package net.erabbit.csl;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,73 +9,160 @@ import static org.junit.Assert.*;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(AndroidJUnit4.class)
 public class CSLTest {
     @Test
-    public void decode_object_empty() throws JSONException {
-        JSONObject config = new JSONObject("{\"type\":\"object\",\"attributes\":[]}");
-        Object result = CSL.decode(new byte[]{}, 0, config, null);
-        assertTrue(result.getClass() == HashMap.class);
-    }
-    @Test
-    public void decode_object_number_uint8() throws JSONException {
+    public void decode_number_uint8() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint8\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFF}, 0, config, null);
         assertEquals(0xFF, result);
     }
     @Test
-    public void decode_object_number_uint16be() throws JSONException {
+    public void decode_number_uint16be() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint16be\"}");
         Object result = CSL.decode(new byte[]{0x00,0x01}, 0, config, null);
         assertEquals(0x0001, result);
     }
     @Test
-    public void decode_object_number_uint16le() throws JSONException {
+    public void decode_number_uint16le() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint16le\"}");
         Object result = CSL.decode(new byte[]{0x00,(byte)0x80}, 0, config, null);
         assertEquals(0x8000, result);
     }
     @Test
-    public void decode_object_number_int16be() throws JSONException {
+    public void decode_number_int16be() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"int16be\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFF,(byte)0xFE}, 0, config, null);
         assertEquals((short)-2, result);
     }
     @Test
-    public void decode_object_number_int16le() throws JSONException {
+    public void decode_number_int16le() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"int16le\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFE,(byte)0xFF}, 0, config, null);
         assertEquals((short)-2, result);
     }
     @Test
-    public void decode_object_number_int32be() throws JSONException {
+    public void decode_number_int32be() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"int32be\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFE}, 0, config, null);
         assertEquals(-2, result);
     }
     @Test
-    public void decode_object_number_int32le() throws JSONException {
+    public void decode_number_int32le() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"int32le\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFE,(byte)0xFF,(byte)0xFF,(byte)0xFF}, 0, config, null);
         assertEquals(-2, result);
     }
     @Test
-    public void decode_object_number_uint32be() throws JSONException {
+    public void decode_number_uint32be() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint32be\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFE}, 0, config, null);
         assertEquals(0xFFFFFFFEL, result);
     }
     @Test
-    public void decode_object_number_uint32le() throws JSONException {
+    public void decode_number_uint32le() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint32le\"}");
         Object result = CSL.decode(new byte[]{(byte)0xFE,(byte)0xFF,(byte)0xFF,(byte)0xFF}, 0, config, null);
         assertEquals(0xFFFFFFFEL, result);
     }
     @Test
-    public void decode_object_number_float32le() throws JSONException {
+    public void decode_number_float32le() throws Exception {
         JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"float32le\"}");
         Object result = CSL.decode(new byte[]{(byte)0x00,(byte)0x00,(byte)0xD0,(byte)0x40}, 0, config, null);
         assertEquals(6.5F, result);
+    }
+    @Test
+    public void decode_number_scale() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"number\",\"numberType\":\"uint8\",\"scale\":0.1}");
+        Object result = CSL.decode(new byte[]{(byte)0x0A}, 0, config, null);
+        assertEquals(1.0, result);
+    }
+    @Test
+    public void decode_string() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"string\",\"byteLength\":5}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{0x48,0x65,0x6C,0x6C,0x6F}, 0, config, report);
+        assertEquals("Hello", result);
+        assertEquals((long)5, (long)report.length);
+    }
+    @Test
+    public void decode_string_padding() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"string\",\"byteLength\":10}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{0x48,0x65,0x6C,0x6C,0x6F,0x00,0x00,0x00,0x00,0x00}, 0, config, report);
+        assertEquals("Hello", result);
+        assertEquals((long)10, (long)report.length);
+    }
+    @Test
+    public void decode_string_hex() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"string\",\"byteLength\":2,\"stringEncoding\":\"hex\"}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{(byte)0xFF,0x12}, 0, config, report);
+        assertEquals("FF12", result);
+        assertEquals((long)2, (long)report.length);
+    }
+    @Test
+    public void decode_bytes() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"bytes\",\"byteLength\":3}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{0x01,0x02,0x03}, 0, config, report);
+        assertArrayEquals(new byte[]{0x01,0x02,0x03}, (byte[])result);
+        assertEquals((long)3, (long)report.length);
+    }
+    @Test
+    public void decode_bytes_no_byteLength() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"bytes\"}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{0x01,0x02,0x03}, 0, config, report);
+        assertArrayEquals(new byte[]{0x01,0x02,0x03}, (byte[])result);
+        assertEquals((long)3, (long)report.length);
+    }
+    @Test
+    public void decode_boolean() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"boolean\"}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object result = CSL.decode(new byte[]{0x01}, 0, config, report);
+        assertEquals(true, result);
+        assertEquals((long)1, (long)report.length);
+    }
+    @Test
+    public void decode_object_bitmask() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"object\",\"objectType\":\"bitmask\",\"attributes\":[{\"name\":\"a\",\"type\":\"boolean\",\"mask\":1},{\"name\":\"b\",\"type\":\"boolean\",\"mask\":2},{\"name\":\"c\",\"type\":\"number\",\"numberType\":\"uint8\",\"mask\":240}]}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Map<String,Object> result = (Map<String,Object>)CSL.decode(new byte[]{0x21}, 0, config, report);
+        assertSame(result.getClass(), HashMap.class);
+        assertEquals(true, result.get("a"));
+        assertEquals(false, result.get("b"));
+        assertEquals(2, result.get("c"));
+    }
+    @Test
+    public void decode_object() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"object\",\"attributes\":[{\"name\":\"n\",\"type\":\"number\",\"numberType\":\"uint8\"},{\"name\":\"s\",\"type\":\"string\",\"byteLength\":3}]}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Map<String,Object> result = (Map<String,Object>)CSL.decode(new byte[]{0x01,0x30,0x31,0x32}, 0, config, report);
+        assertSame(result.getClass(), HashMap.class);
+        assertEquals(1, result.get("n"));
+        assertEquals("012", result.get("s"));
+    }
+    @Test
+    public void decode_object_variable_type() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"object\",\"attributes\":[{\"name\":\"category\",\"type\":\"number\",\"numberType\":\"uint8\",\"values\":[{\"name\":\"book\",\"value\":1},{\"name\":\"movie\",\"value\":2}]},{\"name\":\"info\",\"type\":\"variable\",\"typeIndex\":\"category\",\"types\":[{\"index\":1,\"type\":\"object\",\"attributes\":[{\"name\":\"pages\",\"type\":\"number\",\"numberType\":\"uint8\"}]},{\"index\":2,\"type\":\"object\",\"attributes\":[{\"name\":\"minutes\",\"type\":\"number\",\"numberType\":\"uint8\"}]}]}]}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Map<String,Object> result = (Map<String,Object>)CSL.decode(new byte[]{0x01,(byte)180}, 0, config, report);
+        assertEquals((long)2, (long)report.length);
+        assertSame(result.getClass(), HashMap.class);
+        assertEquals(1, result.get("category"));
+        Map<String,Object> info = (Map<String,Object>)result.get("info");
+        assertEquals(180, info.get("pages"));
+    }
+    @Test
+    public void decode_array() throws Exception {
+        JSONObject config = new JSONObject("{\"type\":\"array\",\"byteLength\":3,\"arrayItem\":{\"type\":\"number\",\"numberType\":\"uint8\"}}");
+        CSL.CSLDecodeReport report = new CSL.CSLDecodeReport();
+        Object[] result = (Object[])CSL.decode(new byte[]{0x01,0x02,0x03}, 0, config, report);
+        assertArrayEquals(new Object[]{1,2,3}, result);
+        assertEquals((long)3, (long)report.length);
     }
 }
--- src/main/java/net/erabbit/csl/CSL.java
+++ src/main/java/net/erabbit/csl/CSL.java
@@ -1,11 +1,11 @@
 package net.erabbit.csl;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,51 +13,211 @@ public class CSL {
 
     public static class CSLDecodeReport {
         public Integer length;
+        CSLDecodeReport() {
+            this.length = 0;
+        }
+        public void checkLength(byte[] data, int offset, int requiredLength) throws Exception {
+            if(data.length >= offset + requiredLength)
+                this.length = requiredLength;
+            else
+                throw new Exception("Data length is not enough");
+        }
     }
-    public static Number decodeNumber(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws JSONException {
+    public static Number decodeNumber(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
         String numberType = config.getString("numberType");
+        Number number;
         switch(numberType) {
             case "uint8":
-                return data[offset] & 0xFF;
+                if(report != null)
+                    report.checkLength(data, offset, 1);
+                number = data[offset] & 0xFF;
+                break;
             case "uint16be":
-                return ((data[offset] & 0xFF) << 8) | (data[offset+1] & 0xFF);
+                if(report != null)
+                    report.checkLength(data, offset, 2);
+                number = ((data[offset] & 0xFF) << 8) | (data[offset+1] & 0xFF);
+                break;
             case "uint16le":
-                return ((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF);
+                if(report != null)
+                    report.checkLength(data, offset, 2);
+                number = ((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF);
+                break;
             case "int16be":
-                return (short)(((data[offset] & 0xFF) << 8) | (data[offset+1] & 0xFF));
+                if(report != null)
+                    report.checkLength(data, offset, 2);
+                number = (short)(((data[offset] & 0xFF) << 8) | (data[offset+1] & 0xFF));
+                break;
             case "int16le":
-                return (short)(((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF));
+                if(report != null)
+                    report.checkLength(data, offset, 2);
+                number = (short)(((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF));
+                break;
             case "uint32be":
-                return ((data[offset] & 0xFFL) << 24) | ((data[offset+1] & 0xFFL) << 16) | ((data[offset+2] & 0xFFL) << 8) | (data[offset+3] & 0xFFL);
+                if(report != null)
+                    report.checkLength(data, offset, 4);
+                number = ((data[offset] & 0xFFL) << 24) | ((data[offset+1] & 0xFFL) << 16) | ((data[offset+2] & 0xFFL) << 8) | (data[offset+3] & 0xFFL);
+                break;
             case "uint32le":
-                return ((data[offset+3] & 0xFFL) << 24) | ((data[offset+2] & 0xFFL) << 16) | ((data[offset+1] & 0xFFL) << 8) | (data[offset] & 0xFFL);
+                if(report != null)
+                    report.checkLength(data, offset, 4);
+                number = ((data[offset+3] & 0xFFL) << 24) | ((data[offset+2] & 0xFFL) << 16) | ((data[offset+1] & 0xFFL) << 8) | (data[offset] & 0xFFL);
+                break;
             case "int32be":
-                return ((data[offset] & 0xFF) << 24) | ((data[offset+1] & 0xFF) << 16) | ((data[offset+2] & 0xFF) << 8) | (data[offset+3] & 0xFF);
+                if(report != null)
+                    report.checkLength(data, offset, 4);
+                number = ((data[offset] & 0xFF) << 24) | ((data[offset+1] & 0xFF) << 16) | ((data[offset+2] & 0xFF) << 8) | (data[offset+3] & 0xFF);
+                break;
             case "int32le":
-                return ((data[offset+3] & 0xFF) << 24) | ((data[offset+2] & 0xFF) << 16) | ((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF);
+                if(report != null)
+                    report.checkLength(data, offset, 4);
+                number = ((data[offset+3] & 0xFF) << 24) | ((data[offset+2] & 0xFF) << 16) | ((data[offset+1] & 0xFF) << 8) | (data[offset] & 0xFF);
+                break;
             case "float32le":
-                return ByteBuffer.wrap(data, offset, 4).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+                if(report != null)
+                    report.checkLength(data, offset, 4);
+                number = ByteBuffer.wrap(data, offset, 4).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+                break;
+            default:
+                throw new Exception("Unknown number type: " + numberType);
         }
-        return null;
+        if(config.has("scale"))
+            return number.doubleValue() * config.getDouble("scale");
+        else
+            return number;
     }
-    public static Map<String,Object> decodeObject(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws JSONException {
+    public static String decodeString(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        if(!config.has("byteLength"))
+            throw new Exception("String config must have byteLength");
+        int byteLength = config.getInt("byteLength");
+        if(report != null)
+            report.checkLength(data, offset, byteLength);
+        if(config.has("stringEncoding") && config.getString("stringEncoding").equals("hex")) {
+            String hexByteConnector = "";
+            if(config.has("hexByteConnector"))
+                hexByteConnector = config.getString("hexByteConnector");
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i=0; i<byteLength; i++) {
+                stringBuilder.append(String.format("%02X", data[offset+i] & 0xFF));
+                if(i<byteLength-1)
+                    stringBuilder.append(hexByteConnector);
+            }
+            return stringBuilder.toString();
+        }
+        else {
+            while(byteLength > 0 && data[offset + byteLength - 1] == 0)
+                byteLength--;
+            return new String(data, offset, byteLength);
+        }
+    }
+    public static byte[] decodeBytes(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        int byteLength;
+        if(config.has("byteLength")) {
+            byteLength = config.getInt("byteLength");
+            if(report != null)
+                report.checkLength(data, offset, byteLength);
+        }
+        else {
+            byteLength = data.length - offset;
+            if(report != null)
+                report.length = byteLength;
+        }
+        byte[] result = new byte[byteLength];
+        System.arraycopy(data, offset, result, 0, byteLength);
+        return result;
+    }
+    public static Boolean decodeBoolean(byte[] data, int offset, CSLDecodeReport report) throws Exception {
+        if(report != null)
+            report.checkLength(data, offset, 1);
+        return data[offset] != 0;
+    }
+    public static Map<String,Object> decodeObject(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        if(config.has("byteLength")) {
+            int byteLength = config.getInt("byteLength");
+            if(report != null)
+                report.checkLength(data, offset, byteLength);
+        }
+        Map<String,Object> result = new HashMap<>();
+        JSONArray attributes = config.getJSONArray("attributes");
+        int totalLength = 0;
+        for(int i=0; i<attributes.length(); i++) {
+            JSONObject attribute = attributes.getJSONObject(i);
+            String attributeName = attribute.getString("name");
+            if(attribute.getString("type").equals("variable")) {
+                String typeIndexKey = attribute.getString("typeIndex");
+                Object typeIndexValue = result.get(typeIndexKey);
+                JSONArray types = attribute.getJSONArray("types");
+                for(int t=0; t<types.length(); t++) {
+                    JSONObject type = types.getJSONObject(t);
+                    if(type.get("index") == typeIndexValue) {
+                        attribute = type;
+                        break;
+                    }
+                }
+            }
+            CSLDecodeReport attributeReport = new CSLDecodeReport();
+            Object attributeValue = decode(data, offset + totalLength, attribute, attributeReport);
+            result.put(attributeName, attributeValue);
+            totalLength += attributeReport.length;
+        }
+        if(report != null)
+            report.length = totalLength;
+        return result;
+    }
+    public static Map<String,Object> decodeBitmask(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        if(report != null)
+            report.checkLength(data, offset, 1);
         Map<String,Object> result = new HashMap<>();
         JSONArray attributes = config.getJSONArray("attributes");
         for(int i=0; i<attributes.length(); i++) {
             JSONObject attribute = attributes.getJSONObject(i);
-            CSLDecodeReport attributeReport = new CSLDecodeReport();
-            Object attributeValue = decode(data, offset, attribute, attributeReport);
-            result.put(attribute.getString("name"), attributeValue);
-            offset += attributeReport.length;
+            String name = attribute.getString("name");
+            int mask = attribute.getInt("mask");
+            int maskShift = 0;
+            while(((1 << maskShift) & mask) == 0)
+                maskShift++;
+            int attrByte = (data[offset] & mask) >> maskShift;
+            Object attrValue = decode(new byte[]{(byte)attrByte}, 0, attribute, null);
+            result.put(name, attrValue);
         }
         return result;
     }
-    public static Object decode(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws JSONException {
+    public static Object[] decodeArray(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        if (!config.has("byteLength"))
+            throw new Exception("Array config must have byteLength");
+        int byteLength = config.getInt("byteLength");
+        if (report != null)
+            report.checkLength(data, offset, byteLength);
+        ArrayList<Object> result = new ArrayList<>();
+        JSONObject arrayItem = config.getJSONObject("arrayItem");
+        CSLDecodeReport itemDecodeReport = new CSLDecodeReport();
+        int totalLength = 0;
+        while (totalLength < byteLength) {
+            itemDecodeReport.length = 0;
+            Object itemValue = decode(data, offset + totalLength, arrayItem, itemDecodeReport);
+            result.add(itemValue);
+            totalLength += itemDecodeReport.length;
+        }
+        return result.toArray();
+    }
+    public static Object decode(byte[] data, int offset, JSONObject config, CSLDecodeReport report) throws Exception {
+        if(offset >= data.length)
+            throw new Exception("offset is larger than data length");
         switch(config.getString("type")) {
             case "number":
                 return decodeNumber(data, offset, config, report);
+            case "string":
+                return decodeString(data, offset, config, report);
+            case "bytes":
+                return decodeBytes(data, offset, config, report);
+            case "boolean":
+                return decodeBoolean(data, offset, report);
             case "object":
-                return decodeObject(data, offset, config, report);
+                if(config.has("objectType") && config.getString("objectType").equals("bitmask"))
+                    return decodeBitmask(data, offset, config, report);
+                else
+                    return decodeObject(data, offset, config, report);
+            case "array":
+                return decodeArray(data, offset, config, report);
         }
         return null;
     }
